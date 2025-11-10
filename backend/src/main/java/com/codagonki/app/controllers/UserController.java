e271e5d5fbc048cc140597bf9350c67aaa62a512
zwaTOx <yakovva36@gmail.com>
2025-11-10 15:58:00 +0700

    add userResponse DTO in signup controller

--- backend/src/main/java/com/codagonki/app/controllers/UserController.java
+++ backend/src/main/java/com/codagonki/app/controllers/UserController.java
@@ -1,15 +1,13 @@
 package com.codagonki.app.controllers;
 
 import com.codagonki.app.DTO.SignupRequest;
+import com.codagonki.app.DTO.UserResponse;
 import com.codagonki.app.models.User;
 import com.codagonki.app.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,16 +20,15 @@ public class UserController {
     }
     
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+    public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
         User user = userService.registerUser(signupRequest);
         
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Пользователь успешно зарегистрирован");
-        response.put("userId", user.getId());
-        response.put("email", user.getEmail());
-        response.put("nickname", user.getNickname());
-        response.put("role", user.getRole());
-        
+        UserResponse response = UserResponse.builder()
+            .userId(user.getId())
+            .email(user.getEmail())
+            .role(user.getRole())
+            .build();
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
 }
