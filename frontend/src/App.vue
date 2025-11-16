5203b9cbdd933260c8740ced4e7736a24214acdd
Эрик <eric.b.aleksandrovich@gmail.com>
2025-11-16 19:27:43 +0700

    add new_front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,23 @@
-<script setup></script>
+<script setup>
+import headerComponent from './components/header.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <headerComponent />
+  <main class="main">
+    <router-view ></router-view>
+  </main>
+  
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  name: 'App',
+  components: { headerComponent },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import profile from '@/views/profile.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/profile',
+      name: 'profile',
+      component: profile,
+    },
+    {
+      // path: '/about',
+      // name: 'about',
+      // // route level code-splitting
+      // // this generates a separate chunk (About.[hash].js) for this route
+      // // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+export default router
