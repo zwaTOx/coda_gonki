860f75394b3a90f84caa3c777d64a17238224ea5
zwaTOx <yakovva36@gmail.com>
2025-11-21 21:47:07 +0700

    add refresh and access token in cookie

--- backend/src/main/java/com/codagonki/app/controllers/UserController.java
+++ backend/src/main/java/com/codagonki/app/controllers/UserController.java
@@ -6,7 +6,13 @@ import com.codagonki.app.DTO.TokenResponse;
 import com.codagonki.app.DTO.UserProfileResponse;
 import com.codagonki.app.DTO.UserResponse;
 import com.codagonki.app.services.UserService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,14 +33,29 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<TokenResponse> loginUser(@Valid @RequestBody LoginRequest loginRequest) {
-        TokenResponse tokenResponse = userService.authenticateUser(loginRequest);
-        return ResponseEntity.ok()
-            .header("alg", "HS256")
-            .header("typ", "JWT")
-            .body(tokenResponse);
-    }
+@PostMapping("/login")
+public ResponseEntity<TokenResponse> loginUser(@Valid @RequestBody LoginRequest loginRequest,
+                                             HttpServletResponse response) { 
+    TokenResponse tokenResponse = userService.authenticateUser(loginRequest);
+    
+    Cookie accessTokenCookie = new Cookie("accessToken", tokenResponse.getAccessToken());
+    accessTokenCookie.setHttpOnly(true);
+    accessTokenCookie.setSecure(true);
+    accessTokenCookie.setPath("/");
+    accessTokenCookie.setMaxAge((int) TimeUnit.MINUTES.toSeconds(30)); 
+    
+    Cookie refreshTokenCookie = new Cookie("refreshToken", tokenResponse.getRefreshToken());
+    refreshTokenCookie.setHttpOnly(true);
+    refreshTokenCookie.setSecure(true);
+    refreshTokenCookie.setPath("/auth/refresh"); 
+    refreshTokenCookie.setMaxAge((int) TimeUnit.DAYS.toSeconds(7)); 
+    response.addCookie(accessTokenCookie);     
+    response.addCookie(refreshTokenCookie);
+    return ResponseEntity.ok()
+        .header("alg", "HS256")
+        .header("typ", "JWT")
+        .body(tokenResponse);
+}
 
     @GetMapping("/me")
     public ResponseEntity<UserProfileResponse> getCurrentUser(@RequestHeader("Authorization") String authorizationHeader) {
