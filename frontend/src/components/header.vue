c196b06bae9318f8dfd779c49018a4acbe2896b0
zwaTOx <yakovva36@gmail.com>
2025-11-25 23:05:25 +0700

    header conflict resolving

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <script setup>
 import HeaderButton from './HeaderButton.vue';
 import HeaderProfileBar from './HeaderProfileBar.vue';
@@ -60,46 +59,3 @@ import Logo from './Logo.vue';
     box-sizing: border-box;
 }
 </style>
-=======
-<template>
-    <header class="headerWrapper">
-        <div class="header">
-            <div class="header__logo">
-                <div class="header__logo--title" :class="{nomode: !mode}">
-                    {{ mode === '' ? 'Codegonks' : `CODE: ` }}
-                </div>
-                <div class="header__logo--mode">{{ `${mode}` }}</div>
-            </div>
-            <div class="header__profile">
-                <div class="header__profile--icon icon">{{ initials }}</div>
-                <div class="header__profile--info">
-                    <div class="header__profile--info--name">Константин Денисов</div>
-                    <div class="header__profile--info--lvl">{{ `${lvl} уровень (${exp})` }}</div>
-                </div>
-            </div>
-        </div>
-    </header>
-</template>
-
-<script>
-export default {
-    data() {
-        return {
-            mode: '',
-            lvl: 2,
-            exp: 240,
-            name: 'Константин Денисов',
-            initials: ''
-        }
-    },
-    methods: {
-        getInitials() {
-            this.initials = this.name.split(' ').map(name => name[0]).join('')
-        }
-    },
-    mounted() {
-        this.getInitials()
-    }
-}
-</script>
->>>>>>> e2704a3536e240f98eae0518fdda14260747537e
