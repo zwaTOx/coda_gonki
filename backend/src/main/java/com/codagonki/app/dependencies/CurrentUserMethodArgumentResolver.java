56a69453b6c810a065aa297c3fe2e7b36b105235
zwaTOx <yakovva36@gmail.com>
2025-11-25 16:44:09 +0700

    Add Duel connect/disconnection

--- backend/src/main/java/com/codagonki/app/dependencies/CurrentUserMethodArgumentResolver.java
+++ backend/src/main/java/com/codagonki/app/dependencies/CurrentUserMethodArgumentResolver.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -21,16 +23,17 @@ public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentR
     }
     
     @Override
-    public boolean supportsParameter(MethodParameter parameter) {
+    public boolean supportsParameter(@NonNull MethodParameter parameter) {
         return parameter.hasParameterAnnotation(CurrentUser.class) && 
                parameter.getParameterType().equals(User.class);
     }
     
     @Override
-    public Object resolveArgument(MethodParameter parameter, 
-                                 ModelAndViewContainer mavContainer,
-                                 NativeWebRequest webRequest, 
-                                 WebDataBinderFactory binderFactory) throws Exception {
+    @Nullable
+    public Object resolveArgument(@NonNull MethodParameter parameter, 
+                                 @Nullable ModelAndViewContainer mavContainer,
+                                 @NonNull NativeWebRequest webRequest, 
+                                 @Nullable WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
         return jwtUtils.getUserFromCookie(request);
     }
